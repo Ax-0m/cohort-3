abb1f69d3279958d6a9123505724dd8d86cc8598
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2025-06-03 18:56:19 +0530

    trying something

---
+++ docker/dodo/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function main(a, b) {
+    return a + b;
+}
+console.log(main(1, 2));
---
+++ docker/dodo/src/index.ts
@@ -0,0 +1,5 @@
+function main(a: number, b: number) {
+  return a + b;
+}
+
+console.log(main(1, 2));
