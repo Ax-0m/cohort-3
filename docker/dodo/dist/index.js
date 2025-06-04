70b0bf73b4a2a5990416721db0793b6fedf95eeb
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2025-06-04 12:09:49 +0530

    docker

--- docker/dodo/dist/index.js
+++ docker/dodo/dist/index.js
@@ -1,5 +1,20 @@
 "use strict";
-function main(a, b) {
-    return a + b;
-}
-console.log(main(1, 2));
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    res.json({
+        message: "Get Endpoint"
+    });
+});
+app.post("/", (req, res) => {
+    res.json({
+        message: "Post Endpoint"
+    });
+});
+app.listen(3000, () => {
+    console.log("Server is running on port 30010");
+});
--- docker/dodo/src/index.ts
+++ docker/dodo/src/index.ts
@@ -1,5 +1,19 @@
-function main(a: number, b: number) {
-  return a + b;
-}
+import express from "express";
 
-console.log(main(1, 2));
+const app =  express();
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "Get Endpoint"
+	})
+})
+
+app.post("/", (req, res) => {
+	res.json({
+		message: "Post Endpoint"
+	})
+})
+
+app.listen(3000, () => {
+	console.log("Server is running on port 3000");
+})
